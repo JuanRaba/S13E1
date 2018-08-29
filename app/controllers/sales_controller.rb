19ea7414047d1250294bf39e1c3ade35b2bca1c6
juanraba <jrabanillog@gmail.com>
2018-08-29 11:33:57 -0300

    add show and link

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,6 +3,11 @@ class SalesController < ApplicationController
   def index
     @sales = Sale.all
   end
+
+  def show
+    @sale = Sale.find(params[:id])
+  end
+
   def new
     @sale = Sale.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'sales', to: 'sales#index'
   get 'sales/new'
   post 'sales', to: 'sales#create'
-  root 'sales#new'
+  get 'sales/:id', to: 'sales#show', as: 'sale'
+
+  root 'sales#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
